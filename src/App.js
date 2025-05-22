2bd4a802fe8a3d37fbf1e438b681680dcdc2dad2
[Noah Murphy] <[noahmurphy47@yahoo.com]>
2025-05-22 08:51:23 -0500

    create and test async thunk, create mock data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useSelector , useDispatch} from 'react-redux';
+import { fetchPostData, parseMock } from './features/posts/postsSlice.js';
+import {useState} from 'react';
 
 function App() {
+  const [mockData, setMockData] = useState([[{data:{children:[{
+  data:{
+    title:'Test Post',
+    ups: 25,
+    downs: 3,
+    num_comments: 100
+  }
+}]}}]]);
+  const dispatch = useDispatch();
   const data = useSelector((state)=>state.posts);
+  
+  const handleClick = () => {
+    dispatch(fetchPostData());
+    
+  }
+  const retrieve = () => {
+    //dispatch(parseMock());
+    const {title, ups, downs, num_comments} = mockData[0][0].data.children[0].data;
+    console.log(title);
+  }
   return (
     <div>
-      <p>{data}</p>
+      <button onClick={()=>{ handleClick()}}></button>
+      <button onClick={()=>{ retrieve()}}> RETRIEVE </button>
+      <p></p>
+
     </div>
   );
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,10 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, current} from '@reduxjs/toolkit';
 
+ export const fetchPostData = createAsyncThunk(
+    'posts/fetchPostData',
+    async(thunkAPI)=>{
+        
+        const response = await fetch('https://www.reddit.com/r/mildlyinfuriating/comments/1krzw9k/received_a_call_from_the_hoa_lawyer_threatening_a.json');
+        const data = await response.json();
+        return data;
+    }
+ )
  const postsSlice = createSlice({
     name: 'posts',
-    initialState: 'hello',
-    reducers:{},
-    
+    initialState: [],
+    reducers:{
+        /*
+        retrieveMock: ((state, action)=>{
+            const result = action.payload.data;
+            state.push(action.payload);
+        }),
+
+        parseMock: ((state)=>{
+
+            const {title, ups, downs, num_comments} = state[0][0].data.children[0].data;
+            console.log(title)
+            return{
+                title,
+                ups,
+                downs,
+                num_comments
+            };
+        })
+        */
+    },
+    extraReducers: builder => {
+        builder.addCase(fetchPostData.pending, () =>{
+            console.log("loading");
+        })
+        builder.addCase(fetchPostData.fulfilled, (state, action) => {
+           console.log("success");
+           state.push(action.payload); 
+           const data = (current(state));
+           console.log(data[0][0].data.children[0].data);
+        })
+        builder.addCase(fetchPostData.rejected, () => {
+            console.log("failed");
+        })
+    }
 })
 
 export const postsReducer = postsSlice.reducer;
+export const { parseMock } = postsSlice.actions;
